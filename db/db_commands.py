c2dc8d5ea486e2a73f0216c88d2c53a0aa4fbe12
Polina Zolotukhina <polinalinana030@gmail.com>
2024-08-08 21:26:58 +0300

    Modified the way to get tables data from the data base in the function 'get_all_students_cards'

--- db/db_commands.py
+++ db/db_commands.py
@@ -92,88 +92,29 @@ async def get_all_students_cards(db):
     :return: Все карты студентов
     """
     try:
-        personal_cols = [PersonalData.id,
-                         PersonalData.firstname,
-                         PersonalData.lastname,
-                         PersonalData.patronymic,
-                         PersonalData.birth_date,
-                         PersonalData.birth_place,
-                         PersonalData.citizenship,
-                         PersonalData.type_of_identity,
-                         PersonalData.address,
-                         PersonalData.marital_status,
-                         PersonalData.snils,
-                         PersonalData.polis,
-                         PersonalData.study_status,
-                         PersonalData.general_status, ]
-
-        educational_cols = [EducationalData.faculty,
-                            EducationalData.direction,
-                            EducationalData.course,
-                            EducationalData.department,
-                            EducationalData.group,
-                            EducationalData.subgroup,
-                            EducationalData.form,
-                            EducationalData.book_num,
-                            EducationalData.degree,
-                            EducationalData.degree_payment,
-                            EducationalData.personal_id]
-
-        contact_cols = [ContactData.number,
-                        ContactData.spare_number,
-                        ContactData.mail,
-                        ContactData.personal_id]
-
-        military_cols = [MilitaryData.status,
-                         MilitaryData.category,
-                         MilitaryData.delay,
-                         MilitaryData.document,
-                         MilitaryData.personal_id]
-
-        benefits_cols = [BenefitsData.benefits,
-                         BenefitsData.personal_id]
-
-        stipend_cols = [StipendData.form,
-                        StipendData.amount,
-                        StipendData.personal_id]
-
-        other_cols = [OtherData.parents,
-                      OtherData.parents_contacts,
-                      OtherData.relatives_works,
-                      OtherData.relatives_addresses,
-                      OtherData.personal_id]
-
         personal_stmt = select(PersonalData)
-        # educational_stmt = (select(*educational_cols))
-        # contact_stmt = (select(*contact_cols))
-        # military_stmt = (select(*military_cols))
-        # benefits_stmt = (select(*benefits_cols))
-        # stipend_stmt = (select(*stipend_cols))
-        # other_stmt = (select(*other_cols))
+        educational_stmt = select(EducationalData)
+        contact_stmt = select(ContactData)
+        military_stmt = select(MilitaryData)
+        benefits_stmt = select(BenefitsData)
+        stipend_stmt = select(StipendData)
+        other_stmt = select(OtherData)
 
         personal_results = await db.execute(personal_stmt)
-        # educational_results = await db.execute(educational_stmt)
-        # contact_results = await db.execute(contact_stmt)
-        # military_results = await db.execute(military_stmt)
-        # benefits_results = await db.execute(benefits_stmt)
-        # stipend_results = await db.execute(stipend_stmt)
-        # other_results = await db.execute(other_stmt)
+        educational_results = await db.execute(educational_stmt)
+        contact_results = await db.execute(contact_stmt)
+        military_results = await db.execute(military_stmt)
+        benefits_results = await db.execute(benefits_stmt)
+        stipend_results = await db.execute(stipend_stmt)
+        other_results = await db.execute(other_stmt)
 
-        # personal_data = [PersonalDataSh.from_orm(row) for row in personal_results.all()]
-        # educational_data = [EducationalDataSh.from_orm(row) for row in educational_results.all()]
-        # contact_data = [ContactDataSh.from_orm(row) for row in contact_results.all()]
-        # military_data = [MilitaryDataSh.from_orm(row) for row in military_results.all()]
-        # benefits_data = [BenefitsDataSh.from_orm(row) for row in benefits_results.all()]
-        # stipend_data = [StipendDataSh.from_orm(row) for row in stipend_results.all()]
-        # other_data = [OtherDataSh.from_orm(row) for row in other_results.all()]
-
-        data = {"personal_data": personal_results.scalars().all(),}
-        #         "educational_data": educational_data,
-        #         "contact_data": contact_data,
-        #         "military_data": military_data,
-        #         "benefits_data": benefits_data,
-        #         "stipend_data": stipend_data,
-        #         "other_data": other_data}
+        data = {"personal_data": personal_results.scalars().all(),
+                "educational_data": educational_results.scalars().all(),
+                "contact_data": contact_results.scalars().all(),
+                "military_data": military_results.scalars().all(),
+                "benefits_data": benefits_results.scalars().all(),
+                "stipend_data": stipend_results.scalars().all(),
+                "other_data": other_results.scalars().all()}
 
         return data
     except Exception as e:
