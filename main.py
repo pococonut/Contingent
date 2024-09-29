40b360426e81b90dac4208869f44d786743498c5
aftelnes <aftelnes@gmail.com>
2024-09-29 17:20:12 +0300

    add cors

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from endpoints import authentication, students_lists, import_cards, student_card, table
 
 
 app = FastAPI(title="Contingent")
+
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(authentication.router)
 app.include_router(students_lists.router)
